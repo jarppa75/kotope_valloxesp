ba1aaf25e150fdc70bddac1bd5d8357721bbeb50
jarppa75 <jarno.juhani.saari@gmail.com>
2024-12-10 16:51:19 +0200

    Create secrets.h

---
+++ valloxesp/secrets.h
@@ -0,0 +1,12 @@
+const char* ota_password = "***";
+
+// wifi settings
+const char* ssid     = "***";
+const char* password = "***";
+
+// mqtt server settings
+const char* mqtt_server   = "192.168.1.*";
+const int mqtt_port       = 1883;
+const char* mqtt_username = "***";
+const char* mqtt_password = "***";
+
